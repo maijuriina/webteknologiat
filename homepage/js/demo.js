a96132ef9c07be7701f6839af960d3da5f9a29e2
maijuriina <maiju.mustonen@gmail.com>
2019-10-04 10:33:46 +0300

    Simple testing completed (and commented out) using demo.js. Calculator functional without using Class.

--- homepage/js/demo.js
+++ homepage/js/demo.js
@@ -23,7 +23,7 @@ function checkNumber() {
     }
 }
 
-function findAddress() {
+function findLocation() {
     var address = document.getElementById("address").value;
     var city = document.getElementById("city").value;
     var src = 'https://www.google.com/maps?q=' + address + city + '&output=embed';
@@ -33,4 +33,52 @@ function findAddress() {
 window.onload = function() {
     var src = 'https://www.google.com/maps?q=Kauppakatu1+Lappeenranta&output=embed';
     document.getElementById('gmap_canvas').src = src;
+    document.getElementById('firstRandom').value = getRandomArbitrary(1, 10);
+    document.getElementById('secondRandom').value = getRandomArbitrary(1, 10);
+}
+
+
+function getRandomArbitrary(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function decreaseValue(randomMinus) {
+    if (document.getElementById(randomMinus).value > 1) {
+        document.getElementById(randomMinus).value--;
+    } 
+}
+
+function increaseValue(randomPlus) {
+    if (document.getElementById(randomPlus).value < 10) {
+        document.getElementById(randomPlus).value++;
+    } 
+}
+
+function calcValues(firstValue, secondValue) {
+    if (document.getElementById('calc-select').value === 'addition')
+    {
+        document.getElementById('result').value = parseInt(document.getElementById(firstValue).value) + 
+        parseInt(document.getElementById(secondValue).value);
+    }
+
+    if (document.getElementById('calc-select').value === 'subtraction')
+    {
+        document.getElementById('result').value = parseInt(document.getElementById(firstValue).value) - 
+        parseInt(document.getElementById(secondValue).value);
+    }
+
+    if (document.getElementById('calc-select').value === 'multiplication')
+    {
+        document.getElementById('result').value = parseInt(document.getElementById(firstValue).value) * 
+        parseInt(document.getElementById(secondValue).value);
+    }
+
+    if (document.getElementById('calc-select').value === 'division')
+    {
+        document.getElementById('result').value = parseInt(document.getElementById(firstValue).value) / 
+        parseInt(document.getElementById(secondValue).value);
+    }
+
 }
