fc5d167b9fd753c148d5d4a8b5f7960dafd0e7e7
maijuriina <maiju.mustonen@gmail.com>
2019-11-20 11:27:33 +0200

    Ajax request for number facts functional. HTML/CSS related issues with button moving upon loading gif appearance.

--- homepage/js/ajaxcode.js
+++ homepage/js/ajaxcode.js
@@ -1,8 +1,27 @@
 var index;
-var numbersUrl: 'https://numbersapi.com/random/trivia';
+var numbersUrl = 'http://numbersapi.com/random/trivia';
 
-$("#loading").css('display', 'none');
+$(document).ready(function () {
+    $("#loading").hide();
+    index = 0;
+})
 
 $("#btn").click(function () {
+    $("#loading").show();
 
+    $.ajax({
+        type: "GET",
+        url: numbersUrl,
+        dataType: "text",
+        success: function (response) {
+            $("#loading").hide();
+            $("#numbersInfo").append("<br><br>" + "<i>" + response + "</i>");
+            index++;
+            $("aside").html("Laskuri: "+ index);
+        },
+        error: function (errorResponse) {
+            $("#loading").hide();
+            console.log("Error msg");
+        }
+    });
 });
