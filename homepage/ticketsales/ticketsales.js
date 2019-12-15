6e45b4c77870c9fc29e7a73e59ac0c29f4664550
maijuriina <maiju.mustonen@gmail.com>
2019-12-15 18:13:43 +0200

    Basic functionality achieved. Button click checks for status discounts using checkboxes and age-field, and determines which discount is best. Prints it on screen along with name. CSS not functional.

--- homepage/ticketsales/ticketsales.js
+++ homepage/ticketsales/ticketsales.js
@@ -1,4 +1,94 @@
-var priceArray_json = '[{"id": 0,"name": "standard","price": 16}, {"id": 1,"name": "halfPrice","price": 8}, {"id": 2,"name": "member","price": 13.6 }, {"id": 3,"name": "student","price": 8.8}, {"id": 4,"name": "studentMember","price": 6.4}, {"id": 5,"name": "free","price": 0}]';
+var priceArray_json = '[{"id": 0,"name": "standard","percentage": 1}, {"id": 1,"name": "halfPrice","percentage": 0.5}, {"id": 2,"name": "member","percentage": 0.85 }, {"id": 3,"name": "student","percentage": 0.55}, {"id": 4,"name": "studentMember","percentage": 0.4}, {"id": 5,"name": "free","percentage": 0}]';
 priceArray = JSON.parse(priceArray_json);
+var standard = priceArray[0];
+var halfPrice = priceArray[1];
+var member = priceArray[2];
+var student = priceArray[3];
+var studentMember = priceArray[4];
+var freeOfCharge = priceArray[5];
+let finalPrice;
+let ageFinalPrice;
+let statusFinalPrice;
 
-// $(".priceCategory").html(priceArray[1].price);
+// fires functions on click of checkboxes
+function checkBoxListener () {
+    $("#student").click(boxControl);
+    $("#conscript").click(boxControl);
+};
+
+// disables conscript or student checkbox if one of the other is checked
+function boxControl () {
+    if (!$("#student").prop("checked")) {
+        $("#conscript").removeAttr("disabled");
+    } else {
+        $("#conscript").attr("disabled", true);
+    }
+
+    if (!$("#conscript").prop("checked")) {
+        $("#student").removeAttr("disabled");
+    } else {
+        $("#student").attr("disabled", true);
+    }
+};
+
+// checks which boxes have been checked and assigns correct statusFinalPrice
+function calculateStatusDiscount () {
+    if ($("#student").prop("checked")) {
+        statusFinalPrice = student.percentage * 16;
+
+    } else if ($("#MTKmember").prop("checked")) {
+        statusFinalPrice = member.percentage * 16;
+
+        if ($("#student").prop("checked")) {
+            statusFinalPrice = studentMember.percentage * 16;
+        }
+
+    } else if ($("#conscript").prop("checked")) {
+        statusFinalPrice = halfPrice.percentage * 16;
+
+    } else {
+        statusFinalPrice = standard.percentage * 16;
+    }
+
+    return statusFinalPrice;
+};
+
+// writes first name on ticket
+$("#firstName").keyup(function() {
+    $(".ticketFirstName").html($("#firstName").val());
+});
+
+// writes last name on ticket
+$("#lastName").keyup(function() {
+    $(".ticketLastName").html(" " + $("#lastName").val());
+});
+
+// checks age discount and assigns it to ageFinalPrice
+function calculateAgeDiscount() {
+    var age = $("#age").val();
+    if (age < 7) {
+        ageFinalPrice = freeOfCharge.percentage * 16;
+    } else if (age >= 7 && age <= 15 || age >= 65) {
+        ageFinalPrice = halfPrice.percentage * 16;
+    } else {
+        ageFinalPrice = standard.percentage * 16;
+    }
+    return ageFinalPrice;
+};
+
+// compares status and age discounts and declares best price as the final price
+function comparePrice (statusFinalPrice, ageFinalPrice) {
+    if (statusFinalPrice > ageFinalPrice) {
+        finalPrice = ageFinalPrice;
+    } else {
+        finalPrice = statusFinalPrice;
+    }
+    $(".priceCategory").html(finalPrice + " €");
+};
+
+// fires all functions at the end of filling the form
+$(".getData").on("click", function () {
+    statusFinalPrice = calculateStatusDiscount();
+    ageFinalPrice = calculateAgeDiscount();
+    comparePrice(statusFinalPrice, ageFinalPrice);
+})
