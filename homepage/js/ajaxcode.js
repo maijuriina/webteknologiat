a250d4776dcc511dde52b49ae96b9f11ba17f2b2
maijuriina <maiju.mustonen@gmail.com>
2019-12-16 13:59:17 +0200

    Contacts Webstorm-based local server through Ajax, fetching all tasks from one button and one id-specified task from another button.

--- homepage/js/ajaxcode.js
+++ homepage/js/ajaxcode.js
@@ -1,10 +1,14 @@
 var index;
+var userIndex;
 var numbersUrl = 'http://numbersapi.com/random/trivia';
 var jokesUrl = 'http://api.icndb.com/jokes/random/1';
+var taskUrl = 'http://localhost:8080/tasks';
+var searchUrl;
 
 $(function () {
     $("#loadingFact").hide();
     $("#loadingJoke").hide();
+    $("#loadingTask").hide();
     index = 0;
 })
 
@@ -48,3 +52,44 @@ $("#jokeFetch").click(function () {
     })
 })
 
+$("#taskFetch").click(function () {
+    $("#loadingTask").show();
+    
+        $.ajax({
+            type: "GET",
+            url: taskUrl,
+            dataType: "json",
+            success: function (response) {
+                $("#loadingTask").hide();
+                // $.each on sama kuin for-loop
+                $.each( response, function (index, taskObject) {
+                    $("#taskDisplay").append("<br><br>" + "<i>" + taskObject.title + ". " + taskObject.description + "</i>");
+                });
+            }, error: function (errorResponse) {
+                $("#loadingTask").hide();
+                console.log(errorResponse);
+            }
+        })
+    })
+
+$("#oneTaskFetch").click(function () {
+    $("#loadingTask").show();
+        userIndex = $("#taskSearch").val();
+        console.log(userIndex);
+        searchUrl = taskUrl + "/" + userIndex;
+        console.log(searchUrl);
+    
+        $.ajax({
+            type: "GET",
+            url: searchUrl,
+            dataType: "text",
+            success: function (response) {
+                $("#loadingTask").hide();
+                $("#oneTaskDisplay").append("<br><br>" + "- " + "<i>" + response + "</i>");
+
+            }, error: function (errorResponse) {
+                $("#loadingTask").hide();
+                console.log(errorResponse);
+            }
+        })
+    })
